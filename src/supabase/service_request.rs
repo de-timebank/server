c9f4faf682c1af6387c1aee4e00aee03b04f9d6f
kari <ammr.arf@gmail.com>
2022-10-27 17:38:34 +0800

    some more changes

--- src/supabase/service_request.rs
+++ src/supabase/service_request.rs
@@ -1,9 +1,9 @@
-use super::rpc::ServiceRequestRpc;
-use super::SupabaseClient;
+use super::{rpc::ServiceRequestRpc, SupabaseClient};
 use crate::proto::timebank::servicerequest::create;
 
 use postgrest::Builder;
 use reqwest::{Error, Response};
+use serde::Serialize;
 use serde_json::json;
 
 pub struct ServiceRequest {
@@ -17,11 +17,14 @@ impl ServiceRequest {
         }
     }
 
-    async fn create(
+    async fn create<T>(
         &self,
-        requestor: &str,
-        request_data: create::TNewServiceRequest,
-    ) -> Result<Response, Error> {
+        requestor: T,
+        request_data: create::NewServiceRequestData,
+    ) -> Result<Response, Error>
+    where
+        T: Serialize,
+    {
         self.client
             .rpc(
                 ServiceRequestRpc::Create,
@@ -35,31 +38,7 @@ impl ServiceRequest {
             .await
     }
 
-    async fn delete(&self, request_id: &str) -> Result<Response, Error> {
-        self.client
-            .rpc(
-                ServiceRequestRpc::Delete,
-                json!({ "_request_id": request_id }).to_string(),
-            )
-            .execute()
-            .await
-    }
-
-    async fn select_bid(&self) -> Result<Response, Error> {
-        self.client
-            .rpc(ServiceRequestRpc::SelectBid, json!({}).to_string())
-            .execute()
-            .await
-    }
-
-    async fn complete_service(&self) -> Result<Response, Error> {
-        self.client
-            .rpc(ServiceRequestRpc::CompleteService, json!({}).to_string())
-            .execute()
-            .await
-    }
-
-    async fn get<T, U>(&self, column: T, filter: U) -> Result<Response, Error>
+    async fn get_by<T, U>(&self, column: T, filter: U) -> Result<Response, Error>
     where
         T: AsRef<str>,
         U: AsRef<str>,
@@ -67,14 +46,73 @@ impl ServiceRequest {
         self.table().eq(column, filter).execute().await
     }
 
-    async fn get_commitment_of<T>(&self, id: T)
+    async fn update<T, U>(&self, id: T, body: U) -> Result<Response, Error>
+    where
+        T: AsRef<str>,
+        U: Into<String>,
+    {
+        self.table().eq("id", id).update(body).execute().await
+    }
+
+    async fn delete<T>(&self, id: T) -> Result<Response, Error>
+    where
+        T: Serialize,
+    {
+        self.client
+            .rpc(
+                ServiceRequestRpc::Delete,
+                json!({ "_request_id": id }).to_string(),
+            )
+            .execute()
+            .await
+    }
+
+    async fn select_bid<T, U>(&self, id: T, bid_id: U) -> Result<Response, Error>
+    where
+        T: Serialize,
+        U: Serialize,
+    {
+        self.client
+            .rpc(
+                ServiceRequestRpc::SelectBid,
+                json!(
+                    {
+                        "_request_id": id,
+                        "_bid_id": bid_id
+                    }
+                )
+                .to_string(),
+            )
+            .execute()
+            .await
+    }
+
+    async fn complete_service<T, U>(&self, id: T, requestor: U) -> Result<Response, Error>
+    where
+        T: Serialize,
+        U: Serialize,
+    {
+        self.client
+            .rpc(
+                ServiceRequestRpc::CompleteService,
+                json!({
+                    "_request_id": id,
+                    "_user_id": requestor,
+                })
+                .to_string(),
+            )
+            .execute()
+            .await
+    }
+
+    async fn get_by_id<T>(&self, id: T) -> Result<Response, Error>
     where
         T: AsRef<str>,
     {
-        todo!()
+        self.get_by("id", id).await
     }
 
-    async fn get_by_id<T>(&self, id: T)
+    async fn get_commitment_of<T>(&self, id: T)
     where
         T: AsRef<str>,
     {
