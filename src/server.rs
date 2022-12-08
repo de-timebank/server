31b373b8173e9b6d41cc2064a96a940b6ade8ba7
Kari Evergreen <evergreenkary@gmail.com>
2022-12-08 15:00:04 +0800

    🔨 refactor: add ctrl-c handler

--- src/server.rs
+++ src/server.rs
@@ -10,6 +10,11 @@ mod services;
 mod starknet;
 mod supabase;
 
+use std::{
+    process::exit,
+    sync::{Arc, Mutex},
+};
+
 use dotenv::dotenv;
 use middleware::RequestLoggerLayer;
 use services::{
@@ -22,6 +27,23 @@ use tonic::transport::Server;
 use tracing::info;
 use tracing_subscriber::{fmt::time::LocalTime, EnvFilter};
 
+fn register_shutdown_handler() {
+    let shutdown_flag = Arc::new(Mutex::new(false));
+
+    ctrlc::set_handler(move || {
+        let mut flag = shutdown_flag.lock().unwrap();
+        let flag_value = *flag;
+
+        if !flag_value {
+            *flag = true;
+            info!("press CTRL-C again to exit...");
+        } else {
+            exit(0)
+        }
+    })
+    .expect("could not register shutdown handler");
+}
+
 fn setup() {
     dotenv().ok();
 
@@ -33,6 +55,8 @@ fn setup() {
         .with_timer(LocalTime::rfc_3339())
         .with_env_filter(EnvFilter::from_default_env())
         .init();
+
+    register_shutdown_handler();
 }
 
 #[tokio::main]
